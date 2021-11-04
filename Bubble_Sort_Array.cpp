c0860d2929569f170d5fa118fb36321b3884888f
Prakriti <prakritimaiti98@gmail.com>
2021-11-04 15:25:16 +0530

    Bubble Sort

--- Bubble_Sort_Array.cpp
+++ Bubble_Sort_Array.cpp
@@ -8,6 +8,19 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    
+    int counter=1;
+    while (counter<n-1){
+        for (int i = 0; i < n-counter; i++){
+            if(arr[i]>arr[i+1]){    //For Assending Order
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        counter++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
